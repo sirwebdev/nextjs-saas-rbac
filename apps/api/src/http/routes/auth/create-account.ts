3bcb9692cc6c1e98b4e92c8f44298c01c50c796e
pmartinsdev <martins20.tech@gmail.com>
2024-05-05 21:11:49 -0300

    feat(api): create user

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
 import { z } from "zod"
+import { hash } from "bcryptjs"
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+import { prisma } from "@/lib/prisma";
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/users', {
@@ -11,7 +14,29 @@ export async function createAccount(app: FastifyInstance) {
         password: z.string().min(6)
       })
     }
-  }, () => {
-    return 'User created!'
+  }, async (request, reply) => {
+    const { name, email, password } = request.body
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email }
+    })
+
+    if (userWithSameEmail) {
+      return reply.status(400).send({
+        message: "user with same email already exists."
+      })
+    }
+
+    const passwordHash = await hash(password, 6)
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash
+      }
+    })
+
+    return reply.status(201).send()
   })
 }
